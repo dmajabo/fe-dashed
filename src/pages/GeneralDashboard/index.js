15ce30fb5653a844ca5342c1441ccc4b4075ef83
Christian Bryant <cbryant@christians-air.lan>
2022-06-15 16:08:06 -0400

    complestes dataset

--- src/pages/GeneralDashboard/index.js
+++ src/pages/GeneralDashboard/index.js
@@ -3,14 +3,6 @@ import { Container } from "reactstrap";
 
 import { Card, CardBody, CardTitle, Col, Row } from "reactstrap";
 
-import {
-  GridContextProvider,
-  GridDropZone,
-  GridItem,
-  swap,
-  move,
-} from "react-grid-dnd";
-
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 import Scatter from "pages/AllCharts/echart/scatterchart";
 import RadarChart from "pages/AllCharts/chartjs/radarchart";
@@ -20,78 +12,11 @@ import BTCPerp from "./btc-perp";
 const GeneralDashboard = () => {
   document.title = "General Dashboard | Dashed by Lacuna";
 
-  const [items, setItems] = React.useState({
-    left: [
-      { id: 1, name: "BTC", component: <BTCCard /> },
-      { id: 2, name: "Avg Funding Rates Over Time", component: <Scatter /> },
-      { id: 3, name: "Buying and Selling Activity", component: <RadarChart /> },
-      {
-        id: 4,
-        name: "Best BTC Perpetual Funding Rates",
-        component: <BTCPerp />,
-      },
-      {
-        id: 5,
-        name: "Bitcoin Monthly Performance",
-        component: <div>..to do</div>,
-      },
-    ],
-  });
-
-  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
-    if (targetId) {
-      const result = move(
-        items[sourceId],
-        items[targetId],
-        sourceIndex,
-        targetIndex
-      );
-      return setItems({
-        ...items,
-        [sourceId]: result[0],
-        [targetId]: result[1],
-      });
-    }
-
-    const result = swap(items[sourceId], sourceIndex, targetIndex);
-    return setItems({
-      ...items,
-      [sourceId]: result,
-    });
-  }
-
   return (
     <>
       <div className="page-content">
         <Container fluid={true}>
           <Breadcrumbs title="Dashboards" breadcrumbItem="General" />
-
-          {/* <Row>
-            <GridContextProvider onChange={onChange}>
-              <div className="container">
-                <GridDropZone
-                  className="dropzone left"
-                  id="left"
-                  boxesPerRow={2}
-                  rowHeight={300}
-                >
-                  {items.left.map(item => (
-                    <GridItem key={item.name}>
-                      <div className="grid-item">
-                        <div className="grid-item-content">
-                          <CardTitle className="mb-4">
-                            {item.name.toUpperCase()}
-                          </CardTitle>
-
-                          <Card>{item.component}</Card>
-                        </div>
-                      </div>
-                    </GridItem>
-                  ))}
-                </GridDropZone>
-              </div>
-            </GridContextProvider>
-          </Row> */}
 
           <Row>
             <Col lg={6}>
