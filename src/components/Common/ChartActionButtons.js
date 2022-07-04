09acb83dca246702924879fb7ce7682ac873f043
Yaroslav Donchenko <donchenkodevyar@gmail.com>
2022-07-04 01:52:50 +0100

    card action buttons update

--- src/components/Common/ChartActionButtons.js
+++ src/components/Common/ChartActionButtons.js
@@ -5,7 +5,7 @@ import close_chart from "../../assets/images/close_chart.svg";
 export default function ChartActionButtons() {
   return (
     <div className="card-actions">
-      <img src={move_chart} />
+      <img src={move_chart} style={{ height: 20 }} />
       <img src={close_chart} />
     </div>
   );
