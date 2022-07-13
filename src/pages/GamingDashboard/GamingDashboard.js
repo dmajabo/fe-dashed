f9bffce486ead051f2cd1a5bb31ce90ee7297ccc
jt-mchorse <jmchorse.tech@gmail.com>
2022-07-13 08:53:30 -0700

    feat(GamingDashboard): add routing

---
+++ src/pages/GamingDashboard/GamingDashboard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import { Container } from "reactstrap";
+
+import PageTitle from 'components/Common/PageTitle';
+import NavTabs, { NavTabItem } from 'components/Common/NavTabs';
+
+import GamingActivity from './GamingActivity';
+import GamingOverview from './GamingOverview';
+import GamingSales from './GamingSales';
+import GamingStatistics from './GamingStatistics';
+
+
+export default function GamingDashboard() {
+  return (
+    <div className="page-content ps-4">
+      <Container fluid>
+        <PageTitle title="Your game metrics" />
+        <NavTabs>
+          <NavTabItem to="/gaming-dashboard/overview" label="Overview" />
+          <NavTabItem to="/gaming-dashboard/sales" label="Sales" />
+          <NavTabItem to="/gaming-dashboard/statistics" label="Statistics" />
+          <NavTabItem to="/gaming-dashboard/activity" label="Activity" />
+        </NavTabs>
+        <Route path="/gaming-dashboard/overview" component={GamingOverview} />
+        <Route path="/gaming-dashboard/sales" component={GamingSales} />
+        <Route path="/gaming-dashboard/statistics" component={GamingStatistics} />
+        <Route path="/gaming-dashboard/activity" component={GamingActivity} />
+      </Container>
+    </div>
+  )
+}
