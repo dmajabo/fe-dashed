018c50651a3981e2602ab6933a9a460ddc75bbad
Ivan <ivandonyk@gmail.com>
2022-08-06 19:51:11 -0400

    fix: dynamic data for butterfly chart

--- src/pages/AllCharts/highcharts/ButterflyChart.js
+++ src/pages/AllCharts/highcharts/ButterflyChart.js
@@ -1,125 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactHighcharts from "react-highcharts";
 import Highcharts from "highcharts";
-
-const config = {
-  title: {
-    text: "Butterfly Chart Example",
-  },
-  subtitle: {
-    text: '<a href="http://stackoverflow.com">stackoverflow.com</a>',
-  },
-  chart: {
-    type: "columnrange",
-    inverted: true,
-    marginTop: 100,
-  },
-  legend: {
-    verticalAlign: "top",
-    y: 60,
-    x: -25,
-    itemDistance: 50,
-  },
-  xAxis: {
-    categories: ["G7", "A8", "V9", "V4", "V3", "V1", "V5"],
-    crossing: 118,
-    lineWidth: 0,
-    tickLength: 0,
-  },
-  yAxis: {
-    gridLineWidth: 0,
-    tickInterval: 50,
-    min: 0,
-    max: 250,
-    // lineWidth: 1,
-    title: {
-      text: null,
-    },
-  },
-  plotOptions: {
-    series: {
-      grouping: false,
-    },
-  },
-  series: [
-    {
-      name: "South",
-      color: "blue",
-      data: [
-        [55, 100],
-        [60, 100],
-        [65, 100],
-        [55, 100],
-        [75, 100],
-        [52, 100],
-        [60, 100],
-      ],
-    },
-    {
-      name: "North",
-      color: "orange",
-      data: [
-        [120, 170],
-        [120, 150],
-        [120, 175],
-        [120, 130],
-        [120, 125],
-        [120, 148],
-        [120, 145],
-      ],
-    },
-  ],
-  //   xAxis: {
-  //     categories: [
-  //       "Jan",
-  //       "Feb",
-  //       "Mar",
-  //       "Apr",
-  //       "May",
-  //       "Jun",
-  //       "Jul",
-  //       "Aug",
-  //       "Sep",
-  //       "Oct",
-  //       "Nov",
-  //       "Dec",
-  //     ],
-  //   },
-  //   series: [
-  //     {
-  //       data: [
-  //         29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1,
-  //         295.6, 454.4,
-  //       ],
-  //     },
-  //   ],
-};
-
-var categories = [
-  "0-4",
-  "5-9",
-  "10-14",
-  "15-19",
-  "20-24",
-  "25-29",
-  "30-34",
-  "35-39",
-  "40-44",
-  "45-49",
-  "50-54",
-  "55-59",
-  "60-64",
-  "65-69",
-  "70-74",
-  "75-79",
-  "80-84",
-  "85-89",
-  "90-94",
-  "95-99",
-  "100 + ",
-];
+import axios from "axios";
+import moment from "moment";
+import _ from "lodash";
 
 export default function ButterflyChart() {
+
+  const [chartData, setChartData] = useState();
+  const categories = [
+    "Janaary-March",
+    "April-June",
+    "July-September",
+    "October-December"
+  ]
+
+  useEffect(() => {
+    const getChartData = async () => {
+      const data = await getButterflyApiData({});
+
+      setChartData(data)
+    };
+    getChartData()
+  }, [])
+
+  const sum = (quarterSumData) => {
+    return quarterSumData.reduce((a, b) => a + b["market_caps"], 0)
+  }
+
+  const getSeriesData = (charDataindex) => {
+    if (chartData) {
+      let seriesData = chartData[Object.keys(chartData)[charDataindex]]
+      let newChartData = []
+      let grouped_items = _.groupBy(seriesData, (b) => moment(b.date).startOf('quarter').format('YYYY/MM/DD'));
+      _.values(grouped_items)
+
+      let allQuarters = Object.keys(grouped_items).map((quarterkey) => {
+        return moment(quarterkey).quarter();
+      })
+
+      categories.forEach((category, index) => {
+        let quarterDate = Object.keys(grouped_items)[index];
+        let quarter = moment(quarterDate).quarter();
+        if (quarterDate && allQuarters.includes(quarter)) {
+          newChartData.push(sum(grouped_items[quarterDate]))
+        } else {
+          newChartData.push(0)
+        }
+      })
+      return newChartData;
+    } else {
+      []
+    }
+  }
+
   return (
     <ReactHighcharts
       config={{
@@ -142,6 +76,7 @@ export default function ButterflyChart() {
         xAxis: [
           {
             categories: categories,
+            // categories: chartData ? Object.keys(chartData) : [],
             reversed: false,
             labels: {
               step: 1,
@@ -171,19 +106,13 @@ export default function ButterflyChart() {
 
         series: [
           {
-            name: "Male",
+            name: "2021",
             color: "#affea2",
-            data: [
-              -2.2, -2.2, -2.3, -2.5, -2.7, -3.1, -3.2, -3.0, -3.2, -4.3, -4.4,
-              -3.6, -3.1, -2.4, -2.5, -2.3, -1.2, -0.6, -0.2, -0.0, -0.0,
-            ],
+            data: getSeriesData(0),
           },
           {
-            name: "Female",
-            data: [
-              2.1, 2.0, 2.2, 2.4, 2.6, 3.0, 3.1, 2.9, 3.1, 4.1, 4.3, 3.6, 3.4,
-              2.6, 2.9, 2.9, 1.8, 1.2, 0.6, 0.1, 0.0,
-            ],
+            name: "2022",
+            data: getSeriesData(1),
           },
         ],
         credits: { enabled: false },
@@ -191,3 +120,39 @@ export default function ButterflyChart() {
     />
   );
 }
+
+export const getButterflyApiData = async ({
+  startDate = "1627776000",
+  endDate = "1659312000",
+  ticker = "bitcoin",
+}) => {
+  const API = `https://api.coingecko.com/api/v3/coins/${ticker}/market_chart/range`;
+
+  try {
+    const { data } = await axios.get(API, {
+      params: {
+        vs_currency: "usd",
+        from: startDate,
+        to: endDate,
+      },
+    });
+    const mappedData = [];
+
+    for (const i in data.prices) {
+      const payload = {
+        price: data.prices[i][1],
+        date: moment(data.prices[i][0]).format("yyyy-MM-DD"),
+        market_caps: data.market_caps[i][1],
+        total_volumes: data.total_volumes[i][1],
+      };
+      mappedData.push(payload);
+    }
+
+    let grouped_items = _.groupBy(mappedData, (b) => moment(b.date).startOf('year').format('YYYY/MM'));
+    _.values(grouped_items)
+
+    return grouped_items;
+  } catch (error) {
+    console.log(error);
+  }
+};
