bdbe5fc4a4e7ae9e759189fd69709f97ff7a92c4
Ivan <ivandonyk@gmail.com>
2022-08-10 00:44:00 +0100

    feat: change butterfly chart image

--- src/components/Common/ChartPicker.js
+++ src/components/Common/ChartPicker.js
@@ -28,6 +28,7 @@ import img2 from "./../../assets/images/charts/bc-2.png";
 import img3 from "./../../assets/images/charts/bc-3.png";
 import img4 from "./../../assets/images/charts/bc-4.png";
 import img5 from "./../../assets/images/charts/bc-5.png";
+import img7 from "./../../assets/images/charts/bc-7.png";
 
 const chart_list = [
   { id: "circle", preview: img0, component: BubbleChart },
@@ -37,7 +38,7 @@ const chart_list = [
   { id: "stacked", preview: img4, component: PolygonFrams },
   { id: "scatter", preview: img5, component: Scatter },
   { id: "sankey", preview: img5, component: SankeyChart },
-  { id: "butterfly", preview: img5, component: ButterflyChart },
+  { id: "butterfly", preview: img7, component: ButterflyChart },
 ];
 
 const templates = [
