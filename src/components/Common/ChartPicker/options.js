1e1be4b51167911d6330e7b367bd4b483a1bfc38
Ivan <ivandonyk@gmail.com>
2022-08-18 21:41:47 +0100

    fix: remove percent multiplication when rendering y-axis data

--- src/components/Common/ChartPicker/options.js
+++ src/components/Common/ChartPicker/options.js
@@ -4,120 +4,36 @@ export const getOption = (type = "bar", data = []) => {
   switch (type) {
     case "bar": // PolygonTransactions
       return {
-        xAxis: [
-          {
-            type: "category",
-            boundaryGap: true,
-            axisTick: {
-              show: false,
+        xAxis: {
+          title: {
+            text: "Sectors",
+            offset: 60,
+            style: {
+              fontWeight: "bold",
+              fontSize: 15,
+              color: "white",
+              fontFamily: "'sequel_100_wide45', sans-serif",
             },
-            axisLabel: {
-              fontWeight: "700",
-              fontSize: 10,
-              lineHeight: 17,
-              color: "#5B6178",
-              inside: true,
-              rotate: 90,
-            },
-            data: data?.map(({ name }) => name),
           },
-        ],
-        yAxis: [
-          {
-            type: "value",
-            axisLine: {
-              show: false,
+          labels: {
+            style: {
+              color: "white",
+              fontFamily: "'sequel_100_wide45', sans-serif",
             },
-            axisLabel: {
-              formatter: "{value}%",
-              fontWeight: "700",
-              fontSize: 12,
-              lineHeight: 24,
-              color: "rgba(255, 255, 255, 0.6)",
-            },
-            axisTick: {
-              show: false,
-            },
-            splitLine: {
-              lineStyle: {
-                color: "rgba(255, 255, 255, 0.2)",
-                type: [2, 2],
-              },
-            },
-            splitNumber: 5,
           },
-          {
-            type: "value",
-            axisLine: {
-              show: false,
-            },
-            axisLabel: {
-              fontWeight: "700",
-              fontSize: 12,
-              lineHeight: 24,
-              color: "rgba(255, 255, 255, 0.6)",
-            },
-            axisTick: {
-              show: false,
-            },
-            splitLine: {
-              show: false,
-            },
-            splitNumber: 5,
-          },
-        ],
+          lineWidth: 0,
+          tickWidth: 0,
+          categories: data?.map(({ name }) => name),
+        },
         series: [
           {
-            name: "Market Cap",
-            type: "bar",
-            xAxisIndex: 0,
-            yAxisIndex: 1,
-            data: data?.map(({ market_cap }) => market_cap),
-            color: {
-              type: "linear",
-              x: 0,
-              y: 0,
-              x2: 0,
-              y2: 1,
-              colorStops: [
-                {
-                  offset: 0,
-                  color: "#36F097",
-                },
-                {
-                  offset: 1,
-                  color: "rgba(54, 240, 151, 0.2)",
-                },
-              ],
-              global: false,
-            },
-          },
-          {
-            name: "Market Cap change 24h",
-            type: "line",
-            smooth: true,
-            symbol: "none",
-            data: data?.map(
-              ({ market_cap_change_24h }) => market_cap_change_24h
-            ),
-            color: {
-              type: "linear",
-              x: 0,
-              y: 0,
-              x2: 0,
-              y2: 1,
-              colorStops: [
-                {
-                  offset: 0,
-                  color: "#B987FD",
-                },
-                {
-                  offset: 1,
-                  color: "#9548FC",
-                },
-              ],
-              global: false,
-            },
+            type: "column",
+            data: data.map(({ name, market_cap, market_cap_change_24h }) => ({
+              y: market_cap_change_24h,
+              name,
+              market_cap,
+              color: market_cap_change_24h > 0 ? "#00C482" : "#FD2249",
+            })),
           },
         ],
       };
@@ -176,7 +92,7 @@ export const getOption = (type = "bar", data = []) => {
     case "bubble": // Scatter
       return {
         grid: {
-            bottom: 40
+          bottom: 40,
         },
         legend: {
           show: true,
@@ -192,7 +108,7 @@ export const getOption = (type = "bar", data = []) => {
             color: "rgba(255, 255, 255, .6)",
             fontFamily: "sequel_100_wide45",
           },
-          nameGap:30,
+          nameGap: 30,
           data: data.map(({ market_cap }) => market_cap),
           axisLine: {
             lineStyle: {
@@ -225,7 +141,7 @@ export const getOption = (type = "bar", data = []) => {
           nameTextStyle: {
             color: "rgba(255, 255, 255, .6)",
             fontFamily: "sequel_100_wide45",
-            align: 'left'
+            align: "left",
           },
           axisLine: {
             lineStyle: {
